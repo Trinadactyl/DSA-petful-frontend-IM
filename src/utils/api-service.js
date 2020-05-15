2ed5a97ce957bf33ce10a1eeb1841f284d6a338d
MediocreIan <idrews094@gmail.com>
2020-05-15 13:58:51 -0500

    more utils added

--- src/utils/api-service.js
+++ src/utils/api-service.js
@@ -3,16 +3,21 @@ import { URL } from "dotenv";
 
 function getPets() {
   fetch(`${URL}/pets`)
+    .then((res) => {
+      if (res.status !== 200) {
+        throw new Error("No more pets probably");
+      }
+    })
     .then((res) => res.json())
     .then((data) => {
       return data;
     });
 }
 
-function adopt(pet) {
-  fetch(`${this.URL}/input`, {
-    method: "POST",
-    body: { type: JSON.stringify(pet) },
+function adopt(petType) {
+  fetch(`${this.URL}/pets`, {
+    method: "DELETE",
+    body: { type: JSON.stringify(petType) },
     headers: {
       "content-type": "application/json",
     },
@@ -22,9 +27,30 @@ function adopt(pet) {
 function getPeople() {
   fetch(`${URL}/people`)
     .then((res) => {
+      if (res.status !== 200) {
+        throw new Error("No more people somehow");
+      }
+    })
+    .then((res) => {
       res.json();
     })
     .then((data) => {
       return data;
     });
+}
+
+function addPerson(newName) {
+  fetch(`${this.URL}/people`, {
+    method: "Post",
+    body: { type: JSON.stringify({ name: newName }) },
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((res) => {
+    if (res.status !== 201) {
+      throw new Error(
+        "that was invalid data Malcom cmon (or my api messed up maybe)"
+      );
+    }
+  });
 }
